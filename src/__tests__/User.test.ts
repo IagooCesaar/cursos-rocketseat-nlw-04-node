ff2516f32408837e835c985fc47375ef7b9fc99d
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2021-02-24 13:59:17 -0300

    Test to verify if same users cheking by email is able to be persisted on db

--- src/__tests__/User.test.ts
+++ src/__tests__/User.test.ts
@@ -3,6 +3,11 @@ import app from '../app'
 
 import createConnection from '../database'
 
+const mockUser = {
+  email: 'user@example.com',
+  name: 'User example'
+}
+
 describe("User's routes", () => {
 
   beforeAll(async () => {
@@ -13,11 +18,16 @@ describe("User's routes", () => {
   it('Should be able to create a new user', async () => {
     const response = await request(app)
       .post('/users')
-      .send({
-        email: 'user@example.com',
-        name: 'User example'
-      });
+      .send(mockUser);
     
     expect(response.status).toBe(201)
+  })
+
+  it('Should not be able to create the same user checking by email', async () => {
+    const response = await request(app)
+      .post('/users')
+      .send(mockUser);
+    
+    expect(response.status).toBe(400)
   })
 })
