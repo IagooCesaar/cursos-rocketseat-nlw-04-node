30e0613758585754241cb0b4a20adc38d6616b7a
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2021-02-26 14:03:04 -0300

    Added yup for validations, validation for user's post route

--- src/controllers/UserController.ts
+++ src/controllers/UserController.ts
@@ -1,10 +1,26 @@
 import { Request, Response } from 'express';
 import { getCustomRepository } from 'typeorm';
 import { UsersRepository } from '../repositories/UserRepository';
+import * as yup from 'yup'
 
 class UserController {
   async create(request: Request, response: Response) {
     const {name, email} = request.body;
+
+    const schema = yup.object().shape({
+      name: yup.string().required("O nome é obrigatório"),
+      email: yup.string().email("O e-mail deverá ser um e-mail válido").required("O e-mail é obrigatório")
+    })
+
+    try {
+      await schema.validate(request.body, {
+        abortEarly: false,
+      })  
+    } catch (error) {
+      return response.status(400).json(error)
+    }
+
+    
 
     const userRepository = getCustomRepository(UsersRepository);
 
