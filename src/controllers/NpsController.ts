da4f6ceb718badf59089229f7bc7da09fe1962b9
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2021-02-26 13:47:40 -0300

    Added Controller and Route to calculate NPS Score

---
+++ src/controllers/NpsController.ts
@@ -0,0 +1,49 @@
+import { Request, Response } from "express";
+import { getCustomRepository, Not, IsNull } from "typeorm";
+import { SurveyUsersRepository } from "../repositories/SurveyUserRepository";
+
+class NpsController { 
+  async execute(req: Request, res: Response) {
+    /*
+      *As notas são respondidas de 1 a 10.
+      *Classificação dos respondentes:
+        Detratores  => 0 a 6
+        Passivos    => 7 a 8
+        Promotores  => 9 a 10
+
+      *Cálculo NPS
+        (Número de Promotores - Número de Detratores) / (Número de respondentes) * 100      
+    */
+
+    const { survey_id } = req.params
+    const surveysUsersRepository = getCustomRepository(SurveyUsersRepository)
+
+    const surveysUsers = await surveysUsersRepository.find({
+      survey_id,
+      value: Not(IsNull())
+    })
+
+    const totalAnswers = surveysUsers.length;
+    const detractors = surveysUsers.filter(survey => (
+      survey.value >=0 && survey.value <= 6
+    )).length
+    const passives = surveysUsers.filter(survey => (
+      survey.value >=7 && survey.value <= 8
+    )).length
+    const promoters = surveysUsers.filter(survey => (
+      survey.value >=9 && survey.value <= 10
+    )).length
+
+    const npsScore = Number((100 * (promoters - detractors) / totalAnswers).toFixed(2));
+
+    return res.status(200).json({
+      detractors,
+      passives,
+      promoters,
+      totalAnswers,
+      npsScore
+    })    
+  }
+}
+
+export { NpsController }
--- src/routes.ts
+++ src/routes.ts
@@ -4,25 +4,29 @@ const router = Router();
 import { UserController } from './controllers/UserController'
 const userController = new UserController();
 
-router.post('/users', userController.create)
-
 import { SurveyController } from './controllers/SurveyController'
 const surveyController = new SurveyController();
-
-router.post('/surveys', surveyController.create)
-router.get('/surveys', surveyController.show)
-
 
 import { SendMailController } from './controllers/SendMailController'
 const sendMailController = new SendMailController();
 
-router.post('/sendMail', sendMailController.execute)
-
-
 import { AnswerController } from './controllers/AnswerController'
 const answerController = new AnswerController();
 
+import { NpsController } from './controllers/NpsController'
+const npsController = new NpsController();
+
+
+router.post('/users', userController.create)
+
+router.post('/surveys', surveyController.create)
+router.get('/surveys', surveyController.show)
+
+router.post('/sendMail', sendMailController.execute)
+
 // http://localhost:3333/answers/1?u=dff8aa5c-0880-487a-a477-804ed0b7926b
 router.get('/answers/:value', answerController.execute)
+
+router.get('/nps/:survey_id', npsController.execute)
 
 export { router }
