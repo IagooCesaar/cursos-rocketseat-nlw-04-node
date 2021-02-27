86e2f09d819183d3064bfb76161d79de171fd123
Iago César Fernandes Nogueira <iagocesar.sgs@gmail.com>
2021-02-27 16:37:40 -0300

    Some mor tests for UserRepository

--- src/__tests__/united/UserRepository.test.ts
+++ src/__tests__/united/UserRepository.test.ts
@@ -3,11 +3,6 @@ import { getConnection, getCustomRepository } from 'typeorm'
 
 import createConnection from '../../database'
 import mockUser1 from './mock/User1.json'
-// const mockUser1 = {
-//   name: "Example User",
-//   email: "user@example.com"
-// }
-
 
 describe("Users Repository", () => {
   
@@ -23,7 +18,6 @@ describe("Users Repository", () => {
   })
 
   it("Should be able to create a new user", async () => {
-    // expect(1).toBe(1)
     const userRepository = getCustomRepository(UsersRepository);
 
     const newUser = userRepository.create(mockUser1)  
@@ -32,5 +26,41 @@ describe("Users Repository", () => {
     const findedUser = await userRepository.findOne(mockUser1)
 
     expect(findedUser).toStrictEqual(newUser)
+  })
+
+  it ('Should be able to find a user already created', async () => {
+    const userRepository = getCustomRepository(UsersRepository)
+
+    const findedUser = await userRepository.findOne(mockUser1)
+    expect(mockUser1).toStrictEqual({
+      name: findedUser.name,
+      email: findedUser.email
+    })
+  })
+
+  it("Should not be able to find and edit user's name", async () => {
+    const userRepository = getCustomRepository(UsersRepository)
+
+    const userAlreadyCreated = await userRepository.findOne(mockUser1);
+    userAlreadyCreated.name = "New User's Name"
+
+    await userRepository.save(userAlreadyCreated)
+
+    const findedUser = await userRepository.findOne({
+      id: userAlreadyCreated.id
+    })
+
+    expect(userAlreadyCreated).toStrictEqual(findedUser)
+  })
+
+  it("Should be able to find and delete a user already created", async () => {
+    const userRepository = getCustomRepository(UsersRepository)
+
+    const user = await userRepository.findOne({email: mockUser1.email})
+    await userRepository.remove(user)
+
+    const userExists = await userRepository.findOne({email: mockUser1.email})
+
+    expect(userExists).toStrictEqual(undefined)
   })
 })
